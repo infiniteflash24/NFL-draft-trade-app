efc40aaf54a3c0c005d5d3155bef1cc433aca061
Anuj Dahiya <89716695+infiniteflash24@users.noreply.github.com>
2023-02-19 14:23:27 -0600

    Adjusted app a bit more

--- server.R
+++ server.R
@@ -94,10 +94,11 @@ function(input, output, session) {
         # geom_hline(yintercept = 0)+
         geom_smooth(se= FALSE, color = 'black')+
         # theme_bw()+
-        theme_wsj(color = "gray")+
-        facet_wrap(. ~ chart_type, scale = 'free')+
-        labs(x = '\nPick number', 
-             y= 'Expected % received in return\n')+
+        theme_wsj()+
+        facet_wrap(. ~ chart_type, 
+                   scale = 'free')+
+        labs(x = '\nPick #', 
+             y= 'Expected % received\n')+
         # xlab(TeX('$Pick\.number$'))+
         # ylab(TeX('$E(Additional amount received in return)$'))+
         # ggtitle('Pick_nun vs the E(Additional amount received) in return.')+
@@ -105,9 +106,84 @@ function(input, output, session) {
               legend.background = element_rect(fill = "white", colour = NA),
               legend.title = element_blank(),
               text = element_text(size=20),
-              axis.title=element_text(size=20),
+              axis.title=element_text(size=30),
               strip.text = element_text(size = 20))+
         scale_y_continuous(labels = scales::percent)
     })
+    
+    output$data <- renderTable({
+      
+      nfl_draft_order%>%
+        filter(Team %in% c(input$Team_1, input$Team_2))%>%
+        # filter(Team %in% c('CHI', 'IND'))
+        transmute(Pick_number, 
+                  gave = Team, 
+                  received = ifelse(gave == input$Team_1, input$Team_2, input$Team_1),
+                  # received = ifelse(Team == 'CHI', 'IND', 'CHI'),
+                  Round, Year, stuart, johnson, hill, otc, pff, Pick_Name)%>%
+        filter(Pick_Name %in% c(input$Team_1_picks, input$Team_2_picks))%>%
+        mutate(trade_id = 40000)%>%
+        group_by(trade_id, gave, received)%>%
+        summarise(stuart = sum(stuart),
+                  johnson = sum(johnson),
+                  hill = sum(hill),
+                  otc = sum(otc),
+                  pff = sum(pff),
+                  min_pick_ev = min(Pick_number),
+                  trade_results = paste(sort(Pick_Name), collapse = ', '))%>%
+        ungroup()%>%
+        gather(chart_type, amount, stuart:pff)%>%
+        left_join(., ., by = c("gave"="received", 
+                               "chart_type" = "chart_type"))%>%
+        transmute(trade_id = trade_id.x,
+                  gave, 
+                  received,
+                  pick_number = min_pick_ev.x,
+                  chart_type, 
+                  amount_given = amount.x,
+                  amount_received = amount.y,
+                  given = trade_results.x,
+                  offered = trade_results.y)%>%
+        group_by(chart_type)%>%
+        filter(pick_number == min(pick_number))%>%
+        mutate(difference = amount_given-amount_received,
+               given_percentage = (amount_given-amount_received)/amount_given,
+               received_percentage = (amount_received-amount_given)/amount_received,
+               season = format(Sys.Date(), "%Y")%>%as.numeric(),
+               trade_date = '2023-04-01',
+               position = 'Suggested trade',
+               pfr_name = 'Suggested trade'
+        )%>%
+        ungroup() -> suggested_trade
+      
+      summary_set%>%
+        filter(amount_received > 0, 
+               amount_given > 0,
+               between(pick_number, input$Pick_num[1], input$Pick_num[2]),
+               # between(pick_number, 1, 32),
+               between(season, input$bins[1], input$bins[2]),
+               # between(season, 2011, 2022),
+               gave %in% input$Gave,
+               received %in% input$Received
+        )%>%
+        bind_rows(suggested_trade)%>%
+        mutate(Difference = -difference,
+               Percentage_gain = -given_percentage,
+               Ratio = amount_received/amount_given
+               # ,
+               # position = ifelse(trade_id == 1648, 'Jameson Williams trade', position)
+        )%>%
+        mutate(chart_type = tools::toTitleCase(chart_type))%>%
+        # select(trade_id:amount_received, ratio, season:pfr_name)%>%
+        brushedPoints(., input$plot_brush, xvar = "pick_number", yvar = "Ratio")
+    })
+    
+    # output$download <- downloadHandler(
+    #   
+    #   filename = function(){"table.csv"}, 
+    #   content = function(fname){
+    #     write.csv(thedata(), fname)
+    #   }
+    # )
 
 }
--- ui.R
+++ ui.R
@@ -1,20 +1,46 @@
 fluidPage(
-
+  tags$head(
+    # Note the wrapping of the string in HTML()
+    tags$style(HTML("
+      @import url('https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap');
+    
+        
+      body {
+        background-color: black;
+        color: orange;
+      }
+      h2 {
+        font-family: 'Yusei Magic', sans-serif;
+      }
+      .shiny-input-container {
+        color: #474747;
+      }"))
+  ),
+  
     titlePanel("NFL Draft Trade Charts"), tags$style(type="text/css", ".recalculating {opacity: 1.0;}"),
     sidebarLayout(
         sidebarPanel(
+            # style = "background-color: green;",
+          
+            chooseSliderSkin("Round"),
+            tags$h2("Trade History"),
+            br(),
             sliderInput("bins",
-                        "NFL Season range",
+                        "Season",
                         min = min(summary_set$season),
                         max = max(summary_set$season),
-                        value = c(2011, max(summary_set$season))
+                        value = c(2011, max(summary_set$season)),
+                        # ticks = FALSE,
+                        round = TRUE
                         ),
             sliderInput("Pick_num",
-                        "Pick number range",
+                        "Pick #",
                         min = min(summary_set$pick_number, na.rm = T),
                         max = max(summary_set$pick_number, na.rm = T),
                         value = c(min(summary_set$pick_number, na.rm = T), 
-                                  32)
+                                  32),
+                        # ticks = FALSE,
+                        round = TRUE
                         ),
             pickerInput("Gave","Team trading down", 
                         choices = summary_set$gave%>%unique()%>%sort(),
@@ -26,13 +52,16 @@ fluidPage(
                         selected = summary_set$received%>%unique(),
                         options = list(`actions-box` = TRUE),
                         multiple = T),
+            br(),
             # submitButton("Update plot\n", icon("refresh")),
             
             # selectInput("Trade_Metric", "Metric",
             #             choices = c('Difference', 'Ratio', 'Percentage_Gain'),
             #             selected = 'ratio',
             #             multiple = FALSE),
-            # textOutput("Suggested trade:\n"),
+            tags$h2("Trade Scenario"),
+            
+            br(),
             selectInput("Team_1", "Team 1",
                         choices = nfl_draft_order$Team%>%unique()%>%sort(),
                         selected = 'CHI',
@@ -48,13 +77,17 @@ fluidPage(
             selectInput("Team_2_picks", "Team 2 picks",
                         choices = nfl_draft_order$Pick_Name%>%unique()%>%sort(),
                         selected = c('IND 2023 1st (#4)', 'IND 2024 1st (#16)', 'IND 2023 2nd (#35)'),
-                        multiple = TRUE),
+                        multiple = TRUE)
+            #, downloadButton('download',"Download table data")
             
-        ),
+        )
+    ,
 
         mainPanel(
-            
-            plotOutput("distPlot", height = '800px')
+            plotOutput("distPlot", height = '700px', brush = 'plot_brush')
+            ,
+            tags$h2("Table"),
+            tableOutput("data")
         )
         
     )
